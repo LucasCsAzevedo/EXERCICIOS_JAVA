53dfad0b78e68f954975dbefd36fd00af4772758
Lucas Azevedo <lucascardoso487@gmail.com>
2024-01-16 16:40:29 -0300

    Commit Inicial

---
+++ EXER1.js
@@ -0,0 +1,3 @@
+var explicacao = "Para atravessar da forma segura, deve-se levar primeiro a cabra para o ponto B, voltar para o A e buscar o maço de alfafa, levá-lo ao ponto B e trazer a cabra para o ponto A, depois pego o Leão e levo para o ponto B, volto para o ponto A e pego a cabra, levando enfim ela e todos os seus pertences seguros para o ponto B"
+
+console.log(explicacao)
